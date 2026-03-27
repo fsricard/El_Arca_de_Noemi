6e91b5435f9e041046a537e1b45367d2bc17f363
Ricard FS <alnaseos@gmail.com>
2026-03-27 15:12:27 +0100

    Añado la ttipografía "Mynerve" al proyecto.

--- includes/header.php
+++ includes/header.php
@@ -54,6 +54,7 @@ require_once(__DIR__ . '/../config/funciones.php');
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Mynerve&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="<?= asset('/css/style.css') ?>" />
         
