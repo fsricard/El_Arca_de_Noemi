f1c14c1bdd61c43d87333101536272c3468e61ed
Ricard FS <alnaseos@gmail.com>
2026-04-09 22:26:42 +0200

    Creo el submódulo "Listado de animales para apadrinar".

--- config/funciones.php
+++ config/funciones.php
@@ -47,6 +47,7 @@ function tituloPagina($pagina) {
         'adopciones_incluir_raza'                   => 'Incluir nuevo animal',
         'noemi_bichillos_listado'                   => 'Listado de los bichillos de Noemí',
         'sistema_adopciones_crear'                  => 'Crear nueva adopción',
+        'apadrina_listado_animales'                 => 'Listado de animales',
         'adopciones_incluir_animal'                 => 'Gestión de Adopciones',
         'sistema_adopciones_iniciar'                => 'Iniciar nueva adopción',
         'sistema_adopciones_por_adoptante'          => 'Adopciones por adoptante',
