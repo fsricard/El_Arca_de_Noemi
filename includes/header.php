afa768c6d406f4b7a8bb54a0c277303e7706254e
Ricard FS <alnaseos@gmail.com>
2026-04-18 22:08:55 +0200

    Repaso y añado los enlaces que faltan en todos los menús del FrontEnd.

--- includes/header.php
+++ includes/header.php
@@ -78,7 +78,9 @@ require_once(__DIR__ . '/../config/funciones.php');
             <nav class="menu-desktop">
                 <ul>
                     <li><a href="<?= asset('/') ?>"><i class="fa-solid fa-house"></i> Inicio</a></li>
-                    <li><a href="<?= asset('/contacto') ?>"><i class="fa-solid fa-envelope"></i> Contacto</a></li>
+                    <li><a href="<?= asset('/listado-adopciones') ?>"><i class="fa-classic fa-solid fa-family"></i> Adopta</a></li>
+                    <li><a href="<?= asset('/listado-apadrinamientos') ?>"><i class="fa-classic fa-solid fa-hands-holding-child"></i> Apadrina</a></li>
+                    <li><a href="<?= asset('/listado-crowdfunding') ?>"><i class="fa-solid fa-hand-holding-dollar"></i> Colabora</a></li>
                 </ul>
             </nav>
 
--- includes/menu_responsive.php
+++ includes/menu_responsive.php
@@ -8,6 +8,7 @@
                         <li><a href="<?= asset('/') ?>"><i class="fa-solid fa-house"></i> Inicio</a></li>
                         <li><a href="<?= asset('/listado-adopciones') ?>"><i class="fa-classic fa-solid fa-family"></i> Adopta</a></li>
                         <li><a href="<?= asset('/listado-apadrinamientos') ?>"><i class="fa-classic fa-solid fa-hands-holding-child"></i> Apadrina</a></li>
+                        <li><a href="<?= asset('/listado-crowdfunding') ?>"><i class="fa-solid fa-hand-holding-dollar"></i> Colabora</a></li>
                         <li><a href="<?= asset('/asi-es-noemi') ?>"><i class="fa-solid fa-user"></i> Sobre mí</a></li>
                         <li><a href="<?= asset('/contacto') ?>"><i class="fa-solid fa-envelope"></i> Contacto</a></li>
                     </ul>
